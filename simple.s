775c47c7a07aaad8bc4665be29a4cb43f14dc249
Raja <rdas6313@gmail.com>
2025-03-30 07:43:32 +0000

    new changes 2

--- simple.s
+++ simple.s
@@ -1,15 +1,27 @@
 .section .vectors
 vector_table:
-    .word 0xABC01200
+    .word _stack_top
     .word reset_handler
     .zero 400
+
+.section .data
+    array: .word 1,2,3,4,5
 
 .section .text
     .align 1
     .type reset_handler, %function
 
 reset_handler:
-    mov r0,#1
-    mov r1,#2
-    add r3,r2,r1
+    ldr r0,=_etext
+    ldr r1,=_sdata
+    ldr r5,=_edata
+loop:
+    ldr r2,[r0]
+    str r2,[r1]
+    add r0,r0,#0x4
+    add r1,r1,#0x4
+    cmp r5,r1
+    bgt loop
+    ldr r6,=array
+    ldr r7,[r6]
     bl .
